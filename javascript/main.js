91f85d13145b8953cb019b50a0377d9f19b4aef4
LeeYongSeok13 <Minamiharuka333@gmail.com>
2023-07-07 15:29:14 +0900

    20230707

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,13 @@
-$(window).scroll
+$(function(){
+    $(document).on('scroll', function(){
+        if($(window).scrollTop() > 78){
+            $("#header").removeClass("on");
+            $("#header").addClass("on");
+        } else {
+            $("#header").addClass("on");
+            $("#header").removeClass("on");
+        }
+    });
+
+});
+
